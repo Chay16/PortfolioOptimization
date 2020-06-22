49ce0076c99fafc892a8ad08ee286c922a941cc2
BouddhaPanda <frederick.sluzaly@gmail.com>
2020-06-22 18:00:44 +0200

    added the Pesaran-Timmermann test function

--- utils.py
+++ utils.py
@@ -21,7 +21,7 @@ def mean_absolute_percentage_error(y_true, y_pred):
 
 def theilU(y_true, y_pred):
     return np.sqrt(np.mean((y_pred - y_true)**2)) / (np.sqrt(np.mean(y_pred**2)) + np.sqrt(np.mean(y_true**2)))
-    
+
     
 # Convert Dataframe to DataLoader
 def DataFrame2DataLoader(df, features_col, target_col, batch_size=4, normalize=False, mu=None, sigma=None):
@@ -48,3 +48,34 @@ def jarque_bera_p_value(x):
 # Augmented Dickey-Fuller, null hypothesis: the serie is stationnary
 def adf_p_value(x):
     return (list(adfuller(x))[1]) #put 0 to get the t stat
+
+# Pesaran-Timmermann test: null hypothesis: the model under study has no power on forecasting the relevant ETF return series
+def PT_test(y_true, y_pred):
+    n = len(y_true)
+
+    dy = y_true.copy()
+    dy[dy < 0] = 0
+    dy[dy > 0] = 1
+
+    py = np.mean(dy)
+    qy = (py*(1-py))/n
+
+    dz = y_pred.copy()
+    dz[dz < 0] = 0
+    dz[dz > 1] = 1
+
+    pz = np.mean(dz)
+    qz = (pz*(1-pz))/n
+
+    p = py*pz + (1-py)*(1-pz)
+
+    v = (p*(1-p))/n
+    w = ((2*py-1)**2)*qz + ((2*pz-1)**2)*qy + 4*qy*qz
+
+    dyz = y_true*y_pred.copy()
+    dyz[dyz < 0] = 0
+    dyz[dyz > 0] = 1
+    pyz = np.mean(dyz)
+
+    PT = (pyz - p)/(v-w)**0.5
+    return(PT)
