Fix Normalisation process and MAPE function

NN.py
@@ -199,7 +199,7 @@ class Model:
         print("Valid MAE : {:.4f} | Valid MAPE  : {:.4f} | Valid RSME : {:.4f} | Valid Theil-U {:.4f}".format(self.validMAE, self.validMAPE, self.validRMSE, self.validTheilU))
         
         
-    def evaluate(self, dataloader, mu, sigma):
+    def evaluate(self, dataloader, mu=None, sigma=None, min_=None, max_=None):
         
         preds, targets = [], []
         self.model.eval()
@@ -214,8 +214,12 @@ class Model:
                     preds += outputs.numpy().T.tolist()[0]
                 targets += target.numpy().tolist()
         
-        targets = (targets + mu) * sigma
-        preds = (preds + mu) * sigma
+        if mu is not None and sigma is not None:
+            targets = (np.array(targets) + mu) * sigma
+            preds = (np.array(preds) + mu) * sigma
+        if min_ is not None and max_ is not None:
+            targets = np.array(targets) * (max_ - min_) + min_
+            preds = np.array(preds) * (max_ - min_) + min_
 
         self.testRMSE = mean_squared_error(targets, preds)
         self.testMAE = mean_absolute_error(targets, preds)
@@ -254,7 +258,7 @@ class Model:
         
         
     # to retrieve the prediction and the target values as list
-    def predict(self, dataloader, mu, sigma):
+    def predict(self, dataloader, mu=None, sigma=None, min_=None, max_=None):
         preds = []
         self.model.eval()
         with torch.no_grad():
@@ -267,4 +271,9 @@ class Model:
                 else:
                     preds += outputs.numpy().T.tolist()[0]
         
-        return(preds)
+        if mu is not None and sigma is not None:
+            preds = (np.array(preds) + mu) * sigma
+        if min_ is not None and max_ is not None:
+            preds = np.array(preds) * (max_ - min_) + min_
+        
+        return preds

utils.py
@@ -17,8 +17,13 @@ def load_file(filename):
 
 def mean_absolute_percentage_error(y_true, y_pred):
 #     y_true[y_true == 0] = 0.000001 # to avoid dividing by 0
-    y_true = y_true[y_true != 0] # to delete when y_true = 0
-    return np.mean(np.abs((y_true - y_pred)/y_true))
+#     y_true = y_true[y_true != 0] # to delete when y_true = 0
+    zeros = np.where(y_true==0)
+    y_truebis = np.delete(y_true, zeros)
+    y_predbis = np.delete(y_pred, zeros)
+    
+#     return np.mean(np.abs((y_true - y_pred)/y_true))
+    return np.mean(np.abs((y_truebis - y_predbis)/y_truebis))
 
 def theilU(y_true, y_pred):
     return np.sqrt(np.mean((y_pred - y_true)**2)) / (np.sqrt(np.mean(y_pred**2)) + np.sqrt(np.mean(y_true**2)))